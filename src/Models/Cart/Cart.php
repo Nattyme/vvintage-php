da0ede7b3e10dc4a8a8923326c641c56e3f3cf41
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-06-24 23:13:15 +0300

    public: add method remove for class Cart

--- src/Models/Cart/Cart.php
+++ src/Models/Cart/Cart.php
@@ -5,7 +5,7 @@ namespace Vvintage\Models\Cart;
 
 final class Cart
 {
-  public static function addToCart ()
+  public static function add ()
   {
     // Пользователь выполнил вход в профиль
     if ( isLoggedIn() ) {
@@ -74,6 +74,53 @@ final class Cart
     }
 
     header('Location: ' . HOST . 'shop/' . $_GET['id']);
+    exit();
+  }
+
+  public static function remove ()
+  {
+    if ( isLoggedIn() ) {
+      // Находим пользователя в БД по id
+      $user = R::load('users', $_SESSION['logged_user']['id']);
+
+      // Получаем корзину из БД
+      $cart = json_decode($user->cart, true);
+
+      // Удаляем товар из корзины
+      unset($cart[$_GET['id']]);
+
+      // Превращаем корзину в json строку
+      $user->cart = json_encode($cart);
+
+      // Обноваляем пользователя в БД
+      R::store($user);
+
+      // Обновляем состояние корзины в сессии
+      $_SESSION['cart'] = $cart;
+
+      // Сообщение об удалении товара
+      $_SESSION['success'][] = ['title' => 'Товар был удалён из корзины.'];
+    }
+
+    if ( !isLoggedIn() ) {
+      if (isset($_COOKIE['cart'])) {
+        // Получаем корзину из COOKIE
+        $cart = json_decode($_COOKIE['cart'], true);
+      } else {
+        $cart = array();
+      }
+
+      // 3. Удаляем товар из корзины
+      unset($cart[$_GET['id']]);
+
+      // 4. Сохранение корзины в COOKIE
+      setcookie('cart', json_encode($cart), time() + 60 * 60 * 24 * 30);
+
+      // Сообщение об удалении товара
+      $_SESSION['success'][] = ['title' => 'Товар был удалён из корзины.'];
+    }
+
+    header('Location: ' . HOST . 'cart');
     exit();
   }
 }
