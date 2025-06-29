c32320d7d721c36d021b3b58316696ed68097eca
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-06-29 17:38:21 +0300

    public: class User base logic

--- src/Controllers/User/UserController.php
+++ src/Controllers/User/UserController.php
@@ -1,7 +1,10 @@
 <?php 
 declare(strict_types=1);
 
+namespace Vvintage\Controllers\User;
+
 final class UserController
 {
   
+
 }
--- src/Models/User/User.php
+++ src/Models/User/User.php
@@ -1,7 +1,44 @@
 <?php 
 declare(strict_types=1);
 
+namespace Vvintage\Models\User;
+
 final class User
 {
-  
+  private int $id;
+  private string $email;
+  private string $name;
+  private string $role;
+
+  public function __construct (array $userData)
+  {
+    $this->id = (int) $userData['id'];
+    $this->email =  $userData['email'];
+    $this->name = $userData['name'];
+    $this->role = $userData['role'] ?? 'user';
+  }
+
+  public function getId(): int {
+    return $this->id;
+  }
+
+  public function getEmail():string
+  {
+    return $this->email;
+  }
+
+  public function getName(): string 
+  {
+    return $this->name;
+  }
+
+  public function isAdmin(): bool
+  {
+    return $this->role === 'admin';
+  }
+
+  public function getRole(): string
+  {
+    return $this->role;
+  }
 }
