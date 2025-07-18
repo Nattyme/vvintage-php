d4aa6cbc0aabcb8530c7b6b53ff02ca432c6cfb8
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-07-18 19:11:34 +0300

    public: lang base messages and interface text

---
+++ src/Lang/ru/buttons.php
@@ -0,0 +1,13 @@
+<?php
+declare(strick_types=1);
+
+return [
+  'button.save' => 'Сохранить',
+  'button.remove' => 'Удалить',
+  'button.submit' => 'Отправить',
+  'button.cancel' => 'Отмена',
+  'button.login' => 'Войти',
+  'button.goto.shop' => 'Перейти в каталог',
+  'button.goto.blog' => 'Перейти в блог',
+  'button.cart.add' => 'Добавить в корзину'
+];
---
+++ src/Lang/ru/messages.php
@@ -0,0 +1,8 @@
+<?php
+declare(strick_types=1);
+
+return [
+  'welcome' => 'Здравствуйте!', 'Вы успешно зарегестрировались на сайте.',
+  'login.success' => 'Здравствуйте!', 'Вы успешно вошли на сайт. Рады снова видеть вас',
+  'login.success.username' => 'Здравствуйте, ' . htmlspecialchars($_SESSION['logged_user']['name']), 'Вы успешно вошли на сайт. Рады снова видеть вас',
+];
