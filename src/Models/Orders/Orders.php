d72eda8fbdf0e448e438635e80859fd266babda7
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-07-19 20:58:55 +0300

    public: new class Orders base logic

---
+++ src/Models/Orders/Orders.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace Vvintage\Models\Orders;
+
+use RedBeanPHP\R;
+use RedBeanPHP\OODBBean;
+
+final class Orders
+{
+  private int $id;
+  private string $name;
+  private string $surname;
+  private string $email;
+  private string $phone;
+  private string $address;
+  private string $timestamp;
+  private string $status;
+  private bool $paid;
+  private array $cart;
+
+  public function __construct(OODBBean $bean)
+  {
+    $this->id = (int) $bean->id;
+    $this->name = $bean->name;
+    $this->surname = $bean->surname;
+    $this->email = $bean->email;
+    $this->phone = $bean->phone;
+    $this->address = $bean->address;
+    $this->timestamp = $bean->timestamp;
+    $this->status = $bean->status;
+    $this->paid = $bean->paid;
+    $this->cart = is_string($bean->cart) ? json_decode($bean->cart ?? '[]', true) : [];
+  }
+
+  
+    public function export(): array
+    {
+      return [
+        'id' => $this->id,
+        'name' => $this->name,
+        'surname' => $this->surname,
+        'email' => $this->email,
+        'phone' => $this->phone,
+        'address' => $this->address,
+        'timestamp' => $this->timestamp,
+        'status' => $this->status,
+        'paid' => $this->paid,
+        'cart' => $this->cart
+      ];
+    }
+   
+}
