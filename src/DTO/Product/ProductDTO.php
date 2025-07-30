4a8d3762a19528a6ce3c5fcc6e1e7e8cd5ff2ae9
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-07-30 21:21:32 +0300

    public: class ProductDTO correction. Add additional vars

--- src/DTO/Product/ProductDTO.php
+++ src/DTO/Product/ProductDTO.php
@@ -8,27 +8,33 @@ final class ProductDTO
 {
   
     private int $id;
+    private string $category;
+    private string $brand;
+    private string $slug;
     private string $title;
     private string $content;
-    private string $brand;
-    private string $url;
-    private string $category;
     private float $price;
+    private string $url;
+    private string $article;
+    private string $stock;
     private string $datetime;
     private ?array $images = null; // изначально изображения не загружены
     private ?int $imagesTotal = null;
+
     public array $translations;
     public string $seoTitle;
     public string $seoDescription;
 
     public function __construct(array $data)
     {
-        $this->id = (int) $data['id'];
-        $this->title = $data['title'];
-        $this->parent_id = (int) $data['parent_id'];
-        $this->image = $data['image'];
-        $this->translations = $data['translations'];
-        $this->seoTitle = $data['seo_title'];
-        $this->seoDescription = $data['seo_description'];
+        $this->id = (int) ($data['id'] ?? null);
+        $this->title = (string) ($data['title'] ?? '');
+        $this->image = (string) ($data['image'] ?? '');
+        $this->translations = is_array($data['translations'] ?? null) ? $data['translations'] : [];
+        $this->seoTitle = (string) ($data['seo_title'] ?? '');
+        $this->seoDescription = (string) ($data['seo_description'] ?? '');
     }
+     
+  
+ 
 }
