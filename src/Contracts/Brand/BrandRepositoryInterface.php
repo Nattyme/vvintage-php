d9e6a712f481568c630fffa349b86bd09db970b3
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-08-16 19:50:42 +0300

    admin: BrandRepository inteface method save param type correction

--- src/Contracts/Brand/BrandRepositoryInterface.php
+++ src/Contracts/Brand/BrandRepositoryInterface.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Vvintage\Contracts\Brand;
 
 use Vvintage\Models\Brand\Brand;
+use Vvintage\DTO\Brand\BrandDTO;
 
 interface BrandRepositoryInterface
 {
     public function getBrandById(int $id): ?Brand;
     public function getAllBrands(): array;
     public function getBrandsByIds(array $ids): array;
-    public function saveBrand(Brand $brand): ?int;
+    public function saveBrand(BrandDTO $brand): ?int;
     public function getAllBrandsCount(?string $sql = null, array $params = []): int;
     public function getBrandsArray(): array;
 }
