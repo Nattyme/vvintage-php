84d021434dd0bfd51d4cce791a98aef722d2234c
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-08-19 21:08:03 +0300

    validation: new class AdminProductImageValidator base logic

---
+++ src/Services/Admin/Validation/AdminProductImageValidator.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Vvintage\Validator\Product;
+
+final class AdminProductImageValidator
+{
+    public static function validate(array $data): array
+    {
+        $errors = [];
+
+        if (!isset($data['filename']) || !is_string($data['filename']) || empty($data['filename'])) {
+            $errors['filename'] = 'Имя файла обязательно';
+        }
+
+        if (!isset($data['product_id']) || !is_int($data['product_id'])) {
+            $errors['product_id'] = 'Номер продукта обязателен и должен быть числом.';
+        }
+
+        if (!isset($data['image_order']) || !is_int($data['image_order'])) {
+            $errors['image_order'] = 'Порядок изображений должен быть числовым';
+        }
+
+        return $errors;
+    }
+}
