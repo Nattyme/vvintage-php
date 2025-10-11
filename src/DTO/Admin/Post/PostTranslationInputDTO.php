f23ded4d52bca8f1ef3a18b3d8b2f5a9e202003c
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-10-11 16:26:55 +0300

    public: new class PostTranslationInputDTO base logic

---
+++ src/DTO/Admin/Post/PostTranslationInputDTO.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Vvintage\DTO\Admin\Post;
+
+final class PostTranslationInputDTO
+{
+    public ?int $post_id;
+    public string $slug;
+    public string $locale;
+    public string $title;
+    public string $description;
+    public string $meta_title;
+    public string $meta_description;
+
+    public function __construct(array $data)
+    {
+
+        $this->post_id = (int) ($data['post_id'] ?? 0);
+        $this->slug = (string) ($data['slug'] ?? '');
+        $this->locale = (string) ($data['locale'] ?? 'ru');
+        $this->title = (string) ($data['title'] ?? '');
+        $this->description = (string) ($data['description'] ?? '');
+        $this->meta_title = (string) ($data['meta_title'] ?? $data['title'] ?? '');
+        $this->meta_description = (string) ($data['meta_description'] ?? $data['description'] ?? '');
+    }
+}
