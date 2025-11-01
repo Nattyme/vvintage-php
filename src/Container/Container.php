1c88e91426e9cc5f94af4015df9e2448c8c54d12
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2025-11-01 12:55:07 +0300

    refactoring: new class Container base logic

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -1,3 +1,31 @@
 <?php
-// TO DO - реализовать логику контейнеров и создавать все зависимости здесь
-// Посмотреть Dependency Injection 
+declare(strict_types=1);
+
+namespace Vvintage\Container;
+
+// Класс контейнера - фабрика для создания других зависимостей 
+class Container {
+  private array $definition = [];
+
+  public function set (string $id, callable $factory): void
+  {
+    $this->definition[$id] = $factory;
+  }
+
+  public function get(string $id)
+  {
+    if (!isset($this->definiton[$id]) ) {
+      throw new \Exeption ("Сервис $id не найден в контейнере");
+    }
+
+    // Если объект уже был создан - возвращаем
+    if (is_object($this->definitions[$id]) && !$this->definitions[$id] instanceof \Closure) {
+      return $this->definitions[$id];
+    }
+
+    //Иначе создаем экземпляр и сохраняем 
+    $this->definitions[$id] = $this->definitions[$id]($this);
+    return $this->definitions[$id];
+
+  }
+}
--- src/Container/services.php
+++ src/Container/services.php
@@ -1,12 +1,12 @@
 <?php
 // Пример
-// $container = new Container();
+$container = new Container();
 
-// $container->set(LocaleService::class, fn() => new LocaleService());
-// $container->set(FlashMessage::class, fn() => new FlashMessage());
-// $container->set(SeoService::class, fn($c) => new SeoService(
-//     $c->get(LocaleService::class),
-//     $c->get(FlashMessage::class)
-// ));
+$container->set(LocaleService::class, fn() => new LocaleService());
+$container->set(FlashMessage::class, fn() => new FlashMessage());
+$container->set(SeoService::class, fn($c) => new SeoService(
+    $c->get(LocaleService::class),
+    $c->get(FlashMessage::class)
+));
 
-// return $container;
+return $container;
